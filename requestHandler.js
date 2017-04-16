984ad9d9606550ccc7110ad5cf3f10ef7f65077c
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-16 19:56:10 +0800

    add getHtml function

--- requestHandler.js
+++ requestHandler.js
@@ -21,8 +21,21 @@ function upload(response) {
 }
 
 function getHtml(response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('./html');
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
   response.end();
 }
 
