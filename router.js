3b27f4160209dd6d3e74df4e1f1f3ca64b116e3c
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-21 16:15:47 +0800

    delete query param

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(pathname, handlers, response, query) {
+function route(pathname, handlers, response) {
   console.log('About to route a request for ' + pathname);
   if (typeof handlers[pathname] == 'function') {
-    handlers[pathname](response, query);
+    handlers[pathname](response);
   } else {
     console.log('No request handler found for ' + pathname);
     response.writeHead(404, {'Content-Tppe': 'text/plain'});
