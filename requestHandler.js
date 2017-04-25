18ba45a9d9f7d47bbf582849d64aa84568e8761d
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-25 22:41:40 +0800

    delete

--- requestHandler.js
+++ requestHandler.js
@@ -33,7 +33,6 @@ function getHtml(response) {
     '</form>'+
     '</body>'+
     '</html>';
-
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
