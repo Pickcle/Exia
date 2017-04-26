992f7db8371236b64dfd301383ca10d3d973196f
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-26 18:26:02 +0800

    index.js

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ handlers['/'] = requestHandlers.start;
 handlers['/start'] = requestHandlers.start;
 handlers['/upload'] = requestHandlers.upload;
 handlers['/getHtml'] = requestHandlers.getHtml;
+handlers['/show'] = requestHandlers.show;
+handlers['/uploadImg'] = requestHandlers.uploadImg;
 
 server.start(router.route, handlers);
