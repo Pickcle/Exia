a7a2aa44cd1a980706d086127713dbc019591c8a
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-26 18:26:11 +0800

    requestHandlers/index.js

--- requestHandlers/index.js
+++ requestHandlers/index.js
@@ -1,7 +1,11 @@
 var startHandler = require('./startHandler.js');
 var uploadHandler = require('./uploadHandler.js');
 var getHtmlHandler = require('./getHtmlHandler.js');
+var showHandler = require('./showHandler.js');
+var uploadImgHandler = require('./uploadImgHandler.js');
 
 exports.start = startHandler.start;
 exports.upload = uploadHandler.upload;
 exports.getHtml = getHtmlHandler.getHtml;
+exports.show = showHandler.show;
+exports.uploadImg = uploadImgHandler.uploadImg;
