ab919e2e72c16405e1481d709c3223f2d2fdd181
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-04-28 10:33:29 +0800

    requestHandlers/startHandler.js

--- requestHandlers/startHandler.js
+++ requestHandlers/startHandler.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-function start(response) {
+function start(request, response) {
   console.log('Request handler \'start\' was called.');
 
   // var content = 'empty';
