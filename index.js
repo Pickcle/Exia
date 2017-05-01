2835aeef5f74e04a66efd78262d15305a3939b38
xiaohuajun <xiaohuajun@hys-inc.cn>
2017-05-01 17:52:49 +0800

    delete route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var handlers = {};
 handlers['/'] = requestHandlers.start;
 handlers['/start'] = requestHandlers.start;
 handlers['/upload'] = requestHandlers.upload;
-handlers['/getHtml'] = requestHandlers.getHtml;
+// handlers['/getHtml'] = requestHandlers.getHtml;
 handlers['/show'] = requestHandlers.show;
 handlers['/uploadImg'] = requestHandlers.uploadImg;
 
