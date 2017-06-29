9e427d565696349837548842bc91ce5da4e4b99f
pickcle <348707253@qq.com>
2017-06-29 16:31:29 +0800

    move the old code into uploadImg

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var server = require('./server.js');
-var router = require('./router.js');
-var requestHandlers = require('./requestHandlers');
-
-var handlers = {};
-handlers['/'] = requestHandlers.start;
-handlers['/start'] = requestHandlers.start;
-handlers['/upload'] = requestHandlers.upload;
-// handlers['/getHtml'] = requestHandlers.getHtml;
-handlers['/show'] = requestHandlers.show;
-handlers['/uploadImg'] = requestHandlers.uploadImg;
-
-server.start(router.route, handlers);
+module.exports = exports = {
+  
+}
---
+++ uploadImg/index.js
@@ -0,0 +1,13 @@
+var server = require('./server.js');
+var router = require('./router.js');
+var requestHandlers = require('./requestHandlers');
+
+var handlers = {};
+handlers['/'] = requestHandlers.start;
+handlers['/start'] = requestHandlers.start;
+handlers['/upload'] = requestHandlers.upload;
+// handlers['/getHtml'] = requestHandlers.getHtml;
+handlers['/show'] = requestHandlers.show;
+handlers['/uploadImg'] = requestHandlers.uploadImg;
+
+server.start(router.route, handlers);
