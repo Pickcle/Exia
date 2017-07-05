2893db6af64bff625603baa0d49a07725496e74b
pickcle <348707253@qq.com>
2017-07-05 14:33:01 +0800

    add mongodb

--- inner-modules/child_process.js
+++ inner-modules/child_process.js
@@ -1,12 +1,12 @@
 var childProcess = require('child_process')
 
-// childProcess.exec('ls -lah', function (error, stdout, stderr) {
-//   console.log(stdout)
-// })
+childProcess.exec('ls -lah', function (error, stdout, stderr) {
+  console.log(stdout)
+})
 
-// var spawn = childProcess.spawn('ls', ['-lah'])
-// spawn.stdout.on('data', function (data) {
-//   console.log('spawn:', data.toString())
-// })
+var spawn = childProcess.spawn('ls', ['-lah'])
+spawn.stdout.on('data', function (data) {
+  console.log('spawn:', data.toString())
+})
 
 var fork = childProcess.fork('inner-modules/console.js')
