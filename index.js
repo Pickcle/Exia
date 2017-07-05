3aac8434c8d610ab18ad3acd5e5ad2b3f6724dcd
pickcle <348707253@qq.com>
2017-07-05 18:04:47 +0800

    add mongodb

--- index.js
+++ index.js
@@ -1,68 +1,7 @@
 var express = require('express')
-var fs = require('fs')
-var path = require('path')
 
 var app = express()
-
 app.use(express.static('client'))
-
-app.get('/', function (req, res) {
-  // res.sendFile('./client/index.html')
-  res.send('get')
+app.listen(5051, function () {
+  console.log('listening: localhost:5051')
 })
-
-app.post('/file_upload', function (req, res) {
-  // console.log(req)
-  // fs.writeFileSync(__dirname + '/test.txt', 'test', function (err) {
-  //   err && console.log(err)
-  //   res.end('end')
-  // })
-})
-
-app.get('/:id', function (req, res) {
-  console.log(req.params)
-  res.send(req.params.id)
-})
-
-app.get('/list', function (req, res) {
-  res.send('list')
-})
-
-app.get('/ab*cd', function (req, res) {
-  res.send('ab*cd')
-})
-
-var server = app.listen(5051, function () {
-  console.log(server.address().address)
-  console.log(server.address().port)
-})
-
-function log(req, res) {
-  console.log('------')
-  console.log('app:', req.app)
-  console.log('baseUrl:', req.baseUrl)
-  console.log('originalUrl:', req.originalUrl)
-  console.log('params:', req.params)
-  console.log('path:', req.path)
-  console.log('protocol:', req.protocol)
-  console.log('query:', req.query)
-  console.log('router:', req.router)
-  console.log('subdomains:', req.subdomains)
-  console.log('hostname:', req.hostname)
-  console.log('accept:', req.accepts())
-  console.log('------')
-  // console.log(res.app)
-  // console.log(res.ap)
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-  // console.log()
-}
