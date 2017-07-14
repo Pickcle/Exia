ee53f6183d22a2f27ae8afb8db40b31093f77dcd
pickcle <348707253@qq.com>
2017-07-14 10:14:07 +0800

    example of find function

--- mongodb/find.js
+++ mongodb/find.js
@@ -4,8 +4,9 @@ var DB_CONN_STR = 'mongodb://localhost:27017/node'
 MongoClient.connect(DB_CONN_STR, function (err, db) {
   console.log('connect succeeded')
   var collection = db.collection('user')
-  collection.find({}).toArray(function (err, result) {
-    console.log('result:', result)
+
+  collection.find({}).toArray(function (err, docs) {
+    console.log('result:', docs)
     db.close()
   })
 })
